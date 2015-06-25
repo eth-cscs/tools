e59ee5cf25d0bf56b5b1cb7f215767bf64902487
perettig <peretti@cscs.ch>
2015-06-25 19:37:31 +0200

    working setup for shared eb install and updated version of h5py

--- easybuild/setup.sh
+++ easybuild/setup.sh
@@ -1,7 +1,7 @@
 #!/usr/local/bin/bash
 
 # Retrieve host name (excluding node number)
-hostName=${HOST//[0-9]/}
+hostName=${HOSTNAME//[0-9]/}
 
 # Check host name
 if [[ "$hostName" != "daint" && "$hostName" != "dora" && "$hostName" != "santis" && "$hostName" != "ela" && "$hostName" != "castor" && "$hostName" != "pilatus" ]] ; then
@@ -12,12 +12,15 @@ fi
 
 # If argument is given, use it as path for easybuild and else use default (/scratch for jenkins or $APPS for the rest)
 if [[ -z "$1" ]]; then
-    if [ $USER == "jenscscs" ] ; then
+    if [[ $USER == "jenscscs" ]] ; then
        export PROJ="/scratch/daint/jenscscs/"$hostName
-    else
+    else # if installing on default apps path, then resulting easyconfig file needs to be pushed to git repository
        export PROJ=$APPS
-#       export EASYBUILD_REPOSITORYPATH=/apps/common/tools/,easybuild/ebfiles_repo/
-#       export EASYBUILD_REPOSITORY=GitRepository
+#       export EASYBUILD_REPOSITORYPATH=git@github.com:eth-cscs/tools.git,easybuild/ebfiles_repo/ # if using private key
+       if [[ -z "$EASYBUILD_REPOSITORYPATH" ]]; then
+           export EASYBUILD_REPOSITORYPATH="https://github.com/eth-cscs/tools.git,easybuild/ebfiles_repo/"$hostName"/"
+       fi
+       export EASYBUILD_REPOSITORY=GitRepository
     fi
 else
     export PROJ=${1}
@@ -28,8 +31,7 @@ echo "Switching to environment modules ..."
 if [[ "$hostName" == "daint" || "$hostName" == "dora" || "$hostName" == "santis" ]] ; then
     source /opt/modules/3.2.10.3/init/bash
     export PATH=/opt/modules/3.2.10.3/bin/:$PATH
-elif ["$hostName" == "castor"]
-then
+elif [[ "$hostName" == "castor" ]] ; then
     export PATH=$PROJ/Modules/$MODULE_VERSION/bin:$PATH
 else #pilatus
     export PATH=/usr/share/Modules/tcl/:$PATH
@@ -39,12 +41,14 @@ fi
 echo "Configuring EasyBuild on `hostname` ..."
 if [[ "$hostName" == "daint" || "$hostName" == "dora" || "$hostName" == "santis" || "$hostName" == "castor" ]] ; then
     export EASYBUILD_MODULES_TOOL=EnvironmentModulesC
-elif [["$hostName" == "ela" || "$hostName" == "pilatus"]
-then
+elif [[ "$hostName" == "ela" || "$hostName" == "pilatus" ]] ; then
     export EASYBUILD_MODULES_TOOL=EnvironmentModulesTcl
 fi
 
 export EASYBUILD_PREFIX=${PROJ}/easybuild
+export EASYBUILD_SET_GID_BIT=1
+#export EASYBUILD_STICKY_BIT=1
+export EASYBUILD_UMASK=002
 export EASYBUILD_BUILDPATH=/dev/shm/$USER
 export EASYBUILD_ROBOT_PATHS=/apps/common/easybuild/cscs_easyconfigs/:
 export EASYBUILD_IGNORE_OSDEPS=0
@@ -53,11 +57,12 @@ if [[ "$hostName" == "daint" || "$hostName" == "dora" || "$hostName" == "santis"
     export EASYBUILD_EXTERNAL_MODULES_METADATA=/apps/common/easybuild/cray_external_modules_metadata.cfg
     export EASYBUILD_EXPERIMENTAL=1
     export EASYBUILD_OPTARCH=$CRAY_CPU_TARGET
-    echo "Unloading PrgEnv-cray (temp. workaround)"
-    module unload PrgEnv-cray
+    echo "Unloading PrgEnv-cray craype cray-mpich "
+    module purge
 fi
 
-export PYTHONPATH=$PYTHONPATH:/apps/common/gitpython/lib/python2.7/site-packages/
+# /apps/common = custom cscs easyblocks
+export PYTHONPATH=$PYTHONPATH:/apps/common:/apps/common/gitpython/lib/python2.7/site-packages/
 env | grep EASYBUILD
 echo $PYTHONPATH
 
