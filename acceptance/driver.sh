800fb44bf42b300800333c92eb41d9636c9631c3
perettig <peretti@cscs.ch>
2015-07-09 20:52:23 +0200

    fix

--- acceptance/driver.sh
+++ acceptance/driver.sh
@@ -4,23 +4,29 @@ source ~/.profile
 
 run_id=$HOSTNAME-$(date +"%m_%d_%Y_%H_%M")
 
-sleep_time=1200
+sleep_time=1
 
 module load cudatoolkit/6.5.14 
 
+echo "===================================="
 echo "Phase one 4 x HPL on "$HOSTNAME 
+echo "===================================="
+
 cd /lus/scratch/perettig/acceptance
 
-echo "\nsalloc -N 12 --exclusive --cpus-per-task=1 --ntasks-per-node=16 --gres=gpu:16 mpirun -n 192 run_linpack.kleinm - on "$HOSTNAME 
+echo "salloc -N 12 --exclusive --cpus-per-task=1 --ntasks-per-node=16 --gres=gpu:16 mpirun -n 192 run_linpack.kleinm - on "$HOSTNAME 
 salloc -N 12 --exclusive --cpus-per-task=1 --ntasks-per-node=16 --gres=gpu:16 mpirun -n 192 run_linpack.kleinm 
 salloc -N 12 --exclusive --cpus-per-task=1 --ntasks-per-node=16 --gres=gpu:16 mpirun -n 192 run_linpack.kleinm 
 salloc -N 12 --exclusive --cpus-per-task=1 --ntasks-per-node=16 --gres=gpu:16 mpirun -n 192 run_linpack.kleinm 
 salloc -N 12 --exclusive --cpus-per-task=1 --ntasks-per-node=16 --gres=gpu:16 mpirun -n 192 run_linpack.kleinm 
 
-echo "sleeping... "$sleep_time"s 
-
+echo "===================================="
+echo "sleeping... "$sleep_time"s"
 sleep $sleep_time
+echo "===================================="
 
-echo "Phase two 40 min HPCG on "$HOSTNAME 2
+echo "Phase two 40 min HPCG on "$HOSTNAME
+echo "===================================="
+
 echo "salloc -N 12 --exclusive --cpus-per-task=1 --ntasks-per-node=16 --gres=gpu:16 mpirun -n 192 run_hpcg.klein - on "$HOSTNAME 
 salloc -N 12 --exclusive --cpus-per-task=1 --ntasks-per-node=16 --gres=gpu:16 mpirun -n 192 run_hpcg.kleinm
