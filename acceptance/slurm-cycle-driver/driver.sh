491bfcfe22f653a3a42ef102cca0d0072fb6e6d7
Guilherme <gppezzi@gmail.com>
2015-07-14 19:48:02 +0200

    Update driver.sh

--- acceptance/slurm-cycle-driver/driver.sh
+++ acceptance/slurm-cycle-driver/driver.sh
@@ -4,7 +4,7 @@ RES1=$(sbatch test1.sh)
 RES2=$(sbatch test2.sh) 
 
 echo $RES1
-srun --dependency=afterok:${RES1##* }:${RES2##* } wait.sh
+srun --dependency=afterany:${RES1##* }:${RES2##* } wait.sh
 
 echo "output run1"
 cat test1.*o test1.*e
