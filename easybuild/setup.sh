09153565e1f7985623d3fe0cc95f14fba1f0c7aa
perettig <peretti@cscs.ch>
2015-09-06 15:36:50 +0200

    clean up/small fixes

--- easybuild/setup.sh
+++ easybuild/setup.sh
@@ -3,10 +3,6 @@
 # Retrieve host name (excluding node number)
 hostName=${HOSTNAME//[0-9]/}
 
-if [[ "$hostName" == "eschaln-" || "$hostName" == "keschln-" ]] ; then
-   export APPS=/apps/escha
-fi
-
 # Check host name
 case "$hostName" in
   "daint" | "dora" | "santis" | "ela" |"castor" | "pilatus" | "brisi" | "monch" | "eschaln-" | "keschln-" )
@@ -22,22 +18,21 @@ esac
 # If argument is given, use it as path for easybuild and else use default (/scratch for jenkins or $APPS for the rest)
 if [[ -z "$1" ]]; then
     if [[ $USER == "jenscscs" ]] ; then
-       if [[ "$hostName" == "daint" || "$hostName" == "dora" || "$hostName" == "santis" || "$hostName" == "brisi" || "$hostName" == "pilatus" ]] ; then
+       if [[ "$hostName" == "daint" || "$hostName" == "dora" || "$hostName" == "santis" || "$hostName" == "brisi" || "$hostName" == "pilatus" || "$hostName" == "monch" ]] ; then
            export PROJ=$SCRATCH"/"$hostName
-       elif [[ "$hostName" == "monch" ]] ; then
-           export PROJ="/mnt/lnec/jenscscs/"$hostName
        elif [[ "$hostName" == "castor" ]] ; then
            export PROJ="/users/jenscscs/sandbox/"$hostName
+           export SCRATCH="/tmp"
        elif [[ "$hostName" == "ela" ]] ; then
            export PROJ="/users/jenscscs/sandbox/"$hostName
        elif [[ "$hostName" == "eschaln-" || "$hostName" == "keschln-" ]] ; then
            export PROJ="/lus/scratch/jenscscs/sandbox/"$hostName
        fi
-    else # if installing on default apps path, then resulting easyconfig file needs to be pushed to git repository
+    else # if installing on default apps path, then resulting easyconfig file needs to be pushed to github repository
        export PROJ=$APPS
        if [[ -z "$EASYBUILD_REPOSITORYPATH" ]]; then
            export EASYBUILD_REPOSITORYPATH="git@github.com:eth-cscs/tools.git,easybuild/ebfiles_repo/"$hostName # if using private key
-#           export EASYBUILD_REPOSITORYPATH="https://github.com/eth-cscs/tools.git,easybuild/ebfiles_repo/"$hostName
+#           export EASYBUILD_REPOSITORYPATH="https://github.com/eth-cscs/tools.git,easybuild/ebfiles_repo/"$hostName # will ask for github password
        fi
        export EASYBUILD_REPOSITORY=GitRepository
     fi
@@ -69,7 +64,6 @@ elif [[ "$hostName" == "ela" ]] ; then
 fi
 
 # Set EasyBuild variables
-echo "Configuring EasyBuild on `hostname` ..."
 if [[ "$hostName" == "daint" || "$hostName" == "dora" || "$hostName" == "santis" || "$hostName" == "castor" || "$hostName" == "brisi" || "$hostName" == "monch" || "$hostName" == "eschaln-" || "$hostName" == "keschln-" ]] ; then
     export EASYBUILD_MODULES_TOOL=EnvironmentModulesC
 elif [[ "$hostName" == "ela" || "$hostName" == "pilatus" ]] ; then
@@ -78,27 +72,26 @@ fi
 
 export EASYBUILD_PREFIX=${PROJ}/easybuild
 export EASYBUILD_SET_GID_BIT=1
-#export EASYBUILD_GROUP_WRITABLE_INSTALLDIR=1
+export EASYBUILD_GROUP_WRITABLE_INSTALLDIR=1
 #export EASYBUILD_STICKY_BIT=1
 export EASYBUILD_UMASK=002
 export EASYBUILD_BUILDPATH=/dev/shm/$USER
 export EASYBUILD_ROBOT_PATHS=/apps/common/easybuild/cscs_easyconfigs/:
 export EASYBUILD_IGNORE_OSDEPS=0
 
+# Set up private repository for jenkins user (it doesnt belong to csstaff group, it is not supposed to write under /apps/common)
 if [[ $USER == "jenscscs" ]] ; then
-    export EASYBUILD_SOURCEPATH=/lus/scratch/jenscscs/sources/
+    export EASYBUILD_SOURCEPATH=$SCRATCH/sources/
 else
     export EASYBUILD_SOURCEPATH=/apps/common/easybuild/sources/
 fi
 
-
-#if [[ "$hostName" == "daint" || "$hostName" == "dora" || "$hostName" == "santis" || "$hostName" == "brisi" || "$hostName" == "eschaln-" || "$hostName" == "keschln-" ]] ; then
 if [[ "$hostName" == "daint" || "$hostName" == "dora" || "$hostName" == "santis" || "$hostName" == "brisi" ]] ; then
     export EASYBUILD_EXTERNAL_MODULES_METADATA=/apps/common/easybuild/cray_external_modules_metadata.cfg
     export EASYBUILD_EXPERIMENTAL=1
     export EASYBUILD_OPTARCH=$CRAY_CPU_TARGET
     export CRAYPE_LINK_TYPE=dynamic
-    echo "Purging modules..."
+#    echo "Purging modules..."
 #    module purge
     module switch PrgEnv-cray PrgEnv-gnu
 fi
@@ -107,18 +100,12 @@ fi
 export PYTHONPATH=$PYTHONPATH:/apps/common:/apps/common/gitpython/lib/python2.7/site-packages/
 env | grep EASYBUILD
 echo PYTHONPATH=$PYTHONPATH
-echo PATH=$PATH
+#echo PATH=$PATH
 
 echo "Updating \$MODULEPATH..."
 mkdir -p $EASYBUILD_PREFIX/modules/all
 module use $EASYBUILD_PREFIX/modules/all
+echo MODULEPATH=$MODULEPATH
 
 echo "Loading EasyBuild..."
-#if [[ $USER == "jenscscs" ]] ; then
-   module load /apps/common/easybuild/modules/all/EasyBuild/2.2.0
-   export EASYBUILD_GROUP_WRITABLE_INSTALLDIR=1
-#else
-#   module load /apps/common/easybuild/modules/all/EasyBuild/2.1.1
-#fi
-
-
+module load /apps/common/easybuild/modules/all/EasyBuild/2.3.0
